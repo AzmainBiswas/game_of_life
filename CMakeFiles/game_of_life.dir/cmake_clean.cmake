d425581800ad4b0a62b2f83f33030466a607da02
AzmainBiswas <azmainbiswas007@gmail.com>
2024-06-19 18:53:19 +0530

    added cmake file

---
+++ CMakeFiles/game_of_life.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game_of_life.dir/src/array.c.obj"
+  "CMakeFiles/game_of_life.dir/src/array.c.obj.d"
+  "CMakeFiles/game_of_life.dir/src/main.c.obj"
+  "CMakeFiles/game_of_life.dir/src/main.c.obj.d"
+  "game_of_life.exe"
+  "game_of_life.exe.manifest"
+  "game_of_life.pdb"
+  "libgame_of_life.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/game_of_life.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
